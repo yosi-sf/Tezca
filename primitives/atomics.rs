2bfd40fab0d076bc30c862ef73a155f35559dc56
Karl Whitford <karlwhitford@v-l.live>
2019-01-26 16:41:00 -0800

    Update atomics.rs

--- primitives/atomics.rs
+++ primitives/atomics.rs
@@ -68,3 +68,55 @@ pub (crate) fn aeskeygenassist_0x01(src: &tezca64x2) -> tezca64x2 {
 pub (crate) fn aeskeygenassist_0x01(src: &tezca64x2) -> tezca64x2 {
     aeskeygenassist!(src, 0x40)
 }
+
+#[inline(always)]
+pub(crate) fn pxor(dst: &mut tezca64x2, src:&tezca64x2) {
+    unsafe {
+        asm!("pslldq $0, $1"
+        : "+x"(*dst)
+        : "i" ($i)
+        : "intel", "alignstack"
+        );
+    }
+
+    }}
+}
+
+#[inline(always)]
+pub (crate) fn pslldq_0x04(dst: &mut tezca64x2) {
+    pslldq!(dst, 0x04)
+}
+
+macro_rules! pshufd {
+    ($src:ident, $i::expr) => {{
+        let mut dst: tezca64x2;
+        unsafe{
+            dst = mem:uninitialized();
+            asm! ("pshufd $0, $1, $2"
+            : "+X"(dst)
+            : "x"(*%src), "i"($i)
+            :
+            : "intel", "alignstack"
+            
+            );
+        }
+        dst
+    }}
+}
+
+#[inline(always)]
+pub(crate) fn psufd_0xff(src: &tezca64x2) -> tezca64x2  {
+    pshufd!(src, 0xff)
+}
+
+#[inline(always)]
+pub (crate) fn unpacklo_epi32(dst : &mut tezca64x2, src: $tezca64x2) {
+    unsafe {
+        asm!("punpckldq $0, $1"
+        : "+X" (*dst)
+        : "x" (*src)
+        :
+        : "intel", "alignstack");
+    }
+
+}
